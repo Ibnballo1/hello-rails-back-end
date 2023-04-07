f0773591d0c326832bf1e42caf4993f5861e3802
Ibnballo1 <belloabdullateef035@gmail.com>
2023-04-07 10:34:41 +0100

    Finish backend

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,4 @@
-class Api::MessagesController < ApplicationController
+class MessagesController < ApplicationController
   before_action :set_message, only: %i[show update destroy]
 
   # GET /messages
