546f8d6ffa873f70712787e3845b08bbc4910f8e
Ibnballo1 <belloabdullateef035@gmail.com>
2023-04-08 08:13:53 +0100

    Fix offenses

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -39,13 +39,14 @@ class MessagesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_message
-      @message = Message.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def message_params
-      params.require(:message).permit(:greeting)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def message_params
+    params.require(:message).permit(:greeting)
+  end
 end
